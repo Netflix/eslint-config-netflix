c89b3f4e479f4176105b93c04b673eaa06b42b8d
Casey <cklebba@netflix.com>
2024-03-11 11:18:48 -0700

    All rules audited for major release (#21)

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 import js from '@eslint/js';
 
+// will be removed by ESLint in a future version
+// see: https://eslint.org/blog/2023/10/deprecating-formatting-rules/
+const deprecated = {
+  'comma-dangle': ['warn', {
+    arrays: 'always-multiline',
+    objects: 'always-multiline',
+    imports: 'always-multiline',
+    exports: 'always-multiline',
+    functions: 'only-multiline',
+  }],
+  'curly': 'warn',
+  'quotes': ['warn', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+  'semi': 'warn',
+};
+
 export default {
   rules: {
     ...js.configs.recommended.rules,
-    'comma-dangle': ['warn', {
-      arrays: 'always-multiline',
-      objects: 'always-multiline',
-      imports: 'always-multiline',
-      exports: 'always-multiline',
-      functions: 'only-multiline',
-    }],
+    ...deprecated,
+    'array-callback-return': 'warn',
     'eqeqeq': 'error',
+    'guard-for-in': 'warn',
+    'no-bitwise': 'warn',
+    'no-caller': 'warn',
     'no-console': 'warn',
     'no-constant-binary-expression': 'error',
-    'no-fallthrough': 'warn',
-    'no-prototype-builtins': 'warn',
+    'no-eval': 'error',
+    'no-extend-native': 'warn',
+    'no-loop-func': 'warn',
+    'no-proto': 'error',
+    'no-sequences': 'warn',
+    'no-script-url': 'error',
     'no-shadow': 'warn',
     'no-undef-init': 'error',
     'no-undef': 'error',
+    'no-unused-expressions': 'warn',
     'no-unused-vars': 'warn',
     'no-var': 'error',
     'prefer-arrow-callback': ['error', { allowNamedFunctions: true }],
     'prefer-const': 'error',
-    'quotes': ['warn', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
-    'semi': 'warn',
+    'prefer-object-has-own': 'error',
   },
   linterOptions: {
     reportUnusedDisableDirectives: true,
